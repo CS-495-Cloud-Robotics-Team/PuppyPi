e889ed668259a0d113b2c0de286806930749850e
ArcherTaylor <101343881+ArcherTaylor@users.noreply.github.com>
2025-01-30 16:14:39 -0600

    Update lambda_function.py
    Adds functionality for retrieving the API key from secrets manager

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,10 +1,38 @@
 import json
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "crawford_openai_api_key"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    client = boto3.client("secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return secret
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+    api_key = get_secret()
+
+    # if api_key is None:
+    #     return {
+    #         "statusCode": 500,
+    #         "body": json.dumps({"error": "Failed to retrieve API key"})
+    #     }
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello world!')
     }
-
-#test
