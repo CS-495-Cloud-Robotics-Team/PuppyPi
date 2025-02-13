f4e303cbb129c3be821cc55e37a4379cd0cc6f0a
Daniel Steuer <dmsteuer@crimson.ua.edu>
2025-02-13 14:53:17 -0600

    Added WindowsWakeWord, example code to be put on raspberryPi that detects a wake word.

---
+++ puppypi/WindowsWakeWord.py
@@ -0,0 +1,79 @@
+import pvporcupine
+import pyaudio
+import numpy as np
+import wave
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#PicoVoice Access Key
+PICO_ACCESS_KEY = os.getenv("PICO_ACCESS_KEY")
+if not PICO_ACCESS_KEY:
+    print(PICO_ACCESS_KEY)
+    raise ValueError("Make sure you have an .env file with PICO_ACCESS_KEY for picovoice.")
+
+#Replace file with custom, to activate this one say "PicoVoice"
+porcupine = pvporcupine.create(access_key=PICO_ACCESS_KEY, keyword_paths=["picovoice_windows.ppn"]) 
+
+pa = pyaudio.PyAudio()
+stream = pa.open(format=pyaudio.paInt16, channels=1, rate=porcupine.sample_rate, 
+                 input=True, frames_per_buffer=porcupine.frame_length)
+
+def record(Audio = pa, Format = pyaudio.paInt16, Channels = 1, Rate = 44100, Chunk = 1024, Duration = 5, Output_Filename = "recorded_audio.wav"):
+    #important inputs are Duration which is the time it records in seconds and Output Filename for how to store it
+    #OpenAI wisper accepts .wav files
+    
+    # Open stream for recording
+    stream = Audio.open(format=Format, channels=Channels, rate=Rate, input=True, frames_per_buffer=Chunk)
+    
+    print("Recording...")
+
+    #This is where the audio is stored
+    frames = []
+    
+    #Record audio in chunks
+    for _ in range(int(Rate / Chunk * Duration)):
+        data = stream.read(Chunk)
+        frames.append(data)
+        
+    
+    print("Recording finished.")
+    
+    #Save the recorded data as a WAV file
+    with wave.open(Output_Filename, 'wb') as wf:
+        wf.setnchannels(Channels)
+        wf.setsampwidth(Audio.get_sample_size(Format))
+        wf.setframerate(Rate)
+        wf.writeframes(b''.join(frames))
+
+    print(f"Saved recording as {Output_Filename}")
+
+print("🎙 Listening...")
+
+try:
+    while True:
+        pcm = stream.read(porcupine.frame_length, exception_on_overflow=False)
+        pcm = np.frombuffer(pcm, dtype=np.int16)
+
+        # Volume test for mic check
+        # volume = np.abs(pcm).mean()
+        # print(f"🔊 Volume Level: {volume:.2f}")
+        
+        #reads in activation word and analizes audio
+        result = porcupine.process(pcm.tolist())
+        # if word is detected result will be >=0
+        if result >= 0:
+            print("🔥 Wake word detected!")
+            # Function to listen and save the next "Duration" seconds of audio
+            record()
+            # Break for testing purposes, in real program this can be deleted to rerun
+            break
+            # Add a function to send audio to cloud and call the program?
+
+except KeyboardInterrupt:
+    print("Stopping...")
+    stream.close()
+    pa.terminate()
+    porcupine.delete()
+
