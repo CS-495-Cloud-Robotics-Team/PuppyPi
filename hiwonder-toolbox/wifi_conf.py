cb7215d2e2467f68c154c02d1fe631f6d02ad918
ArcherTaylor <101343881+ArcherTaylor@users.noreply.github.com>
2025-04-08 14:56:26 -0500

    Changes wifi_conf to a .env import

--- hiwonder-toolbox/wifi_conf.py
+++ hiwonder-toolbox/wifi_conf.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 #coding:utf8
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 WIFI_MODE = 2                               #wifi的工作模式， 1为AP模式， 2为STA模式
 #WIFI_AP_SSID = 'HW-Robot'                  #AP模式下的SSID。字符和数字构成,需要以 HW- 开头，否则app功能无法使用
 #WIFI_AP_PASSWORD = 'hiwonder'        #AP模式下的WIFI密码,字符和数字构成
-WIFI_STA_SSID = 'HTIL Router'            #STA模式下的SSID
-WIFI_STA_PASSWORD = '59056195'    #STA模式下的WIFI密码  
+WIFI_STA_SSID = os.getenv('WIFI_STA_SSID', 'default_ssid')            #STA模式下的SSID
+WIFI_STA_USERNAME = os.getenv('WIFI_STA_USERNAME', 'default_username')
+WIFI_STA_PASSWORD = os.getenv('WIFI_STA_PASSWORD', 'default_password')    #STA模式下的WIFI密码  
