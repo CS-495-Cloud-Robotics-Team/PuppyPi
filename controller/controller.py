db82150cebfc631be06e7947fe5b41d0fd81325c
Daniel Steuer <dmsteuer@crimson.ua.edu>
2025-04-17 16:51:30 -0500

    got rid of stop everything

--- controller/controller.py
+++ controller/controller.py
@@ -181,19 +181,19 @@ def record():
 
     return processed_audio_buffer
 
-def stop_everything():
-    ws.send(json.dumps(payloads_dict.get("stop-face-detect")))
-    ws.send(json.dumps(payloads_dict.get("stop-visual-patrol")))
-    ws.send(json.dumps(payloads_dict.get("stop-visual-patrol")))
-    ws.send(json.dumps(payloads_dict.get("stop-color-detect")))
-    # print(f"this is stop color detect function              {payloads_dict.get("stop-color-detect")}")
-    print("stopping everything !!!!!!!!!!!!!!!!!!!!!!!!!!!")
+# def stop_everything():
+#     ws.send(json.dumps(payloads_dict.get("stop-face-detect")))
+#     ws.send(json.dumps(payloads_dict.get("stop-visual-patrol")))
+#     ws.send(json.dumps(payloads_dict.get("stop-visual-patrol")))
+#     ws.send(json.dumps(payloads_dict.get("stop-color-detect")))
+#     # print(f"this is stop color detect function              {payloads_dict.get("stop-color-detect")}")
+#     print("stopping everything !!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 def record_and_process():
     print("Wake word detected!")
     
     stop_event.set()
-    stop_everything()
+    # stop_everything()
     print("full stopping!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     if not (command_queue.empty()):
         while not command_queue.empty():
